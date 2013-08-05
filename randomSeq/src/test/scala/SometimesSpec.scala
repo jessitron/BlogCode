f53d5511706f0b06a069cc396a8e9244eb73a90a
Jessica Kerr <jessitron@gmail.com>
2013-08-05 00:11:10 -0500

    Now the test is happy

--- randomSeq/src/test/scala/SometimesSpec.scala
+++ randomSeq/src/test/scala/SometimesSpec.scala
@@ -1,11 +1,14 @@
 import org.scalatest._
+import scala.util.Random
 
 class SometimesSpec extends FunSpec with ShouldMatchers {
 
+  def evenDistribution(n: Int) = Random.shuffle(Range(0,n).map(1.0/n*_))
 
   describe("doing things sometimes") {
     it("returns true 40% of the time") {
-      val sometimes = new Sometimes(0.4)
+      val notSoRandom = evenDistribution(100).iterator
+      val sometimes = new Sometimes(0.4, notSoRandom.next())
 
       def results = Stream.continually(sometimes.shouldI).take(100)
 
