30eedb4a8454acb24f65ae5688dbb002b2365999
Jessica Kerr <jessitron@gmail.com>
2013-09-13 16:43:31 -0500

    More formatting

--- applicativeFunctor/src/main/scala/ApplicativeFunctor.scala
+++ applicativeFunctor/src/main/scala/ApplicativeFunctor.scala
@@ -3,9 +3,9 @@ package jessitron.af
 object RetrieveAll {
   def retrieveAll: Seq[DocID] => DbTask[Seq[Document]] = { ids =>
     val seqOfOps = ids map DbTask.retrieveOne
-    val wrappedEmpty: DbTask[Seq[Document]] = DbTask.of(Seq.empty[Document])
-    def wrappedPrepend = DbTask.of((b: Seq[Document]) => (a: Document) => a +: b)
+    val wrappedEmpty: DbTask[Seq[Document]] = DbTask of Seq.empty[Document]
+    def wrappedPrepend = DbTask of ((b: Seq[Document]) => (a: Document) => a +: b)
 
-    seqOfOps.foldRight(wrappedEmpty){(e, soFar) => e(soFar(wrappedPrepend))}
+    seqOfOps.foldRight(wrappedEmpty){(e, soFar) => e apply (soFar apply wrappedPrepend)}
   }
 }
