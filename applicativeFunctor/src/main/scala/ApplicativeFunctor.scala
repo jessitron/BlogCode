fae1249625f83391e7321d9c92452f45381585b5
Jessica Kerr <jessitron@gmail.com>
2013-09-13 16:54:13 -0500

    Refactor: pull out turnInsideOut method

--- applicativeFunctor/src/main/scala/ApplicativeFunctor.scala
+++ applicativeFunctor/src/main/scala/ApplicativeFunctor.scala
@@ -3,8 +3,12 @@ package jessitron.af
 object RetrieveAll {
   def retrieveAll: Seq[DocID] => DbTask[Seq[Document]] = { ids =>
     val seqOfOps = ids map DbTask.retrieveOne
-    val wrappedEmpty: DbTask[Seq[Document]] = DbTask of Seq.empty[Document]
-    def wrappedPrepend = DbTask of ((b: Seq[Document]) => (a: Document) => a +: b)
+    turnInsideOut(seqOfOps)
+  }
+
+  private def turnInsideOut[X]: Seq[DbTask[X]] => DbTask[Seq[X]] = { seqOfOps =>
+    val wrappedEmpty: DbTask[Seq[X]] = DbTask of Seq.empty[X]
+    def wrappedPrepend = DbTask of ((b: Seq[X]) => (a: X) => a +: b)
 
     seqOfOps.foldRight(wrappedEmpty){(e, soFar) => e apply (soFar apply wrappedPrepend)}
   }
